bcd91f59f0f6fed9505b07e32611b07ec006ae09
Manuel Palacio <emanuel.palacio@gmail.com>
2014-01-06 08:22:34 +0100

    updated ds file

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -46,20 +46,5 @@ environments {
                 databaseName = "hFa5sSA3XB0qB9gNzfQ"
             }
         }
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=false
-               validationQuery="SELECT 1"
-               jdbcInterceptors="ConnectionState"
-            }
-        }
     }
 }
