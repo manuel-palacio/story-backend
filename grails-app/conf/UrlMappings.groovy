0f59ad1bac7eb3e990d8d2230b45821277e31f95
Manuel Palacio <emanuel.palacio@gmail.com>
2014-03-17 18:38:35 +0100

    cleaned up

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-        "/$controller/$action?/$id?(.${format})?"{
-            constraints {
-                // apply constraints here
-            }
-        }
 
         "/auth/$action"(controller: "login", parseRequest: true)
 
